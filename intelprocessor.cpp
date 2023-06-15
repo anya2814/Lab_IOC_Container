f96a04cb9875649dc2ca87864c02fcae6c50603f
Anya Klimenko <anyaklimenko@inbox.ru>
2023-06-15 14:35:12 +0700

    Удаление условия в методе SetProcessor

--- intelprocessor.cpp
+++ intelprocessor.cpp
@@ -9,10 +9,7 @@ IntelProcessor::IntelProcessor()
 
 void IntelProcessor::SetProcessor(std::string Version_, ProcessorType Type_, double Speed_)
 {
-    Version = Version_; Speed = Speed_;
-    if (Type == 64 || Type == 86)
-        Type = Type_;
-    else std::cout << "Incorrect processor type." << std::endl;
+    Version = Version_; Speed = Speed_; Type = Type_;
 }
 
 std::string IntelProcessor::GetProcessorInfo()
